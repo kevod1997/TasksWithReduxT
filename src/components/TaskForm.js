bcd21dc8bc4c2ac28bf15a5cf8eb6f9c46d20ad4
Kevin Defalco <kevindefalco@gmail.com>
2022-11-17 17:25:27 -0300

    redux practice

--- src/components/TaskForm.js
+++ src/components/TaskForm.js
@@ -1,5 +1,9 @@
 
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
+import {useDispatch,useSelector} from 'react-redux'
+import {addTask, editTask} from '../features/tasks/taskSlice'
+import {v4 as uuid} from 'uuid'
+import { useNavigate, useParams } from "react-router-dom";
 
 function TaskForm() {
 const [task, setTask] = useState({
@@ -14,17 +18,57 @@ const handleChange = e => {
     });
 }
 
+const dispatch = useDispatch()
+const navigate = useNavigate();
+const params = useParams();
+const tasks = useSelector((state) => state.tasks);
+
 const handleSubmit= (e) => {
     e.preventDefault()
-    console.log(task);
+    if (params.id) {
+        dispatch(editTask({ ...task, id: params.id }));
+      } else {
+        dispatch(
+          addTask({
+            ...task,
+            id: uuid(),
+          })
+        );
+      }
+  
+      navigate("/");
 }
+useEffect(() => {
+    if (params.id) {
+      setTask(tasks.find((task) => task.id === params.id));
+    }
+  }, [params, tasks]);
 
-  return (<form onSubmit={handleSubmit}>
-    <input name='title' type='text' placeholder='title' onChange={handleChange}/>
-    <textarea name='description' placeholder="descrption" onChange={handleChange}></textarea>
-
-    <button>Save</button>
-  </form>
+  return (
+  <form onSubmit={handleSubmit} className="bg-zinc-800 max-w-sm p-4">
+  <label className="block text-sm font-bold">Task:</label>
+  <input
+    type="text"
+    name="title"
+    onChange={handleChange}
+    value={task.title}
+    className="w-full p-2 rounded-md bg-zinc-600 mb-2"
+    placeholder="Write a title"
+    autoFocus
+  />
+  <label>
+    Description:
+    <textarea
+      type="text"
+      name="description"
+      onChange={handleChange}
+      value={task.description}
+      className="w-full p-2 rounded-md bg-zinc-600 mb-2"
+      placeholder="Write a description"
+    />
+  </label>
+  <button type="submit" className="bg-indigo-600 px-2 py-1">Submit</button>
+</form>
   )
 }
 
--- src/features/tasks/taskSlice.js
+++ src/features/tasks/taskSlice.js
@@ -1,30 +1,47 @@
-import {createSlice} from '@reduxjs/toolkit'
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = [
-    {
-        id: '1',
-        title: 'task 1',
-        description: 'task 1 description',
-        completed: false
-    },
-    {
-        id: '2',
+  {
+    id: "1",
+    title: "task 1",
+    description: "task 1 description",
+    completed: false,
+  },
+  {
+    id: "2",
 
-        title: 'task 2',
+    title: "task 2",
 
-        description: 'task 2 description',
+    description: "task 2 description",
 
-        completed: false
-
-    }
-]
+    completed: false,
+  },
+];
 
 export const taskSlice = createSlice({
-    name: 'tasks',
-    initialState,
-    reducers: {
+  name: "tasks",
+  initialState,
+  reducers: {
+    addTask: (state, action) => {
+      state.push(action.payload);
+    },
+    editTask: (state, action) => {
+        const { id, title, description } = action.payload;
+        const foundTask = state.find((task) => task.id === id);
+        if (foundTask) {
+          foundTask.title = title;
+          foundTask.description = description;
+        }
+      },
+    deleteTask: (state, action) => {
+      const taskFound = state.find((task) => task.id === action.payload);
+      if (taskFound) {
+        state.splice(state.indexOf(taskFound), 1);
+      }
+    },
+  },
+});
 
-    }
-})
+export const { addTask, deleteTask, editTask } = taskSlice.actions;
 
-export default taskSlice.reducer
+export default taskSlice.reducer;
